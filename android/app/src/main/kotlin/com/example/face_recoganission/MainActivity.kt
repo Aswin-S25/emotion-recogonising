54911ddc455049bb035b3c5aefc2ee8813d9e0be
Aswin S <aswinsaswin25@gmail.com>
2023-03-08 23:21:02 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/face_recoganission/MainActivity.kt
@@ -0,0 +1,7 @@
+package com.example.face_recoganission
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+    
+}
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:camera/camera.dart';
+import 'home.dart';
+
+List<CameraDescription>? cameras;
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  cameras = await availableCameras();
+  runApp(MyApp());
+}
+  
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primaryColor: Colors.deepPurple),
+      home: Home(),
+    );
+  }
+}
